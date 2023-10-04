e3606b032059125cd31a8e32c4c5b9b402fb2ec2
Pontus Gustafsson <gustafsson.pontus@live.se>
2023-10-04 09:18:34 +0200

    Parallellization working

--- lib/matrix_utils.c
+++ lib/matrix_utils.c
@@ -20,7 +20,31 @@ double **create_matrix(size_t n_rows, size_t n_columns) {
   return as;
 }
 
+float **create_matrixf(size_t n_rows, size_t n_columns) {
+  // Creates a (row major) matrix of size n_rows * n_columns with all elements
+  // set to 0 max_val controls how large the random elements can be (-max_val,
+  // max_val)
+  float **as = (float **)malloc(sizeof(float *) * n_rows);
+  float *asentries = (float *)malloc(sizeof(float) * n_rows * n_columns);
+
+  for (size_t i = 0; i < n_rows; ++i) {
+    as[i] = asentries + i * n_columns;
+  }
+
+  for (size_t i = 0; i < n_rows; ++i) {
+    for (size_t j = 0; j < n_columns; ++j) {
+      as[i][j] = 0;
+    }
+  }
+
+  return as;
+}
 void free_matrix(double **matrix) {
+  // Free memory allocated for matrix by create_matrix function
+  free(matrix[0]);
+  free(matrix);
+}
+void free_matrixf(float **matrix) {
   // Free memory allocated for matrix by create_matrix function
   free(matrix[0]);
   free(matrix);
--- lib/matrix_utils.h
+++ lib/matrix_utils.h
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 
 double **create_matrix(size_t n_rows, size_t n_columns);
+float **create_matrixf(size_t n_rows, size_t n_columns);
 void free_matrix(double **matrix);
+void free_matrixf(float **matrix);
 
 #endif
